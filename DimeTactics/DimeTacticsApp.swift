79591b1025f7816c4a8e7041acb1cafedf7857be
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-01-18 19:09:53 -0600

    Initial Commit

---
+++ DimeTactics/DimeTacticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DimeTacticsApp.swift
+//  DimeTactics
+//
+//  Created by J. DeWeese on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct DimeTacticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
